a2915b0cabb00a501d8b992a2f9f4598dcb0b3b1
전영서 <20wjsdudtj@gmail.com>
2024-03-12 13:49:16 +0900

    Referee에서 스트라이크, 볼, 낫싱 판단 strike, ball 두 개로 예상되는 결과이므로 int 리스트 형태 사용

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -1,12 +1,30 @@
 package baseball;
 
-public class Referee {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
-    /*
-     * 3 스트라이크 상황에서 player에게 입력값을 요청한다. -> 게임을 계속 할지 중단할지 결정한다.
-     */
+import static baseball.Constants.GAME_RANDOM_COUNT;
+
+public class Referee {
 
     /*
      * 스트라이크, 볼, 낫싱을 판단한다.
      */
+    public int[] compare(ArrayList<Integer> gameNumList, ArrayList<Integer> playerNumList){
+        int strike = 0;
+        int ball = 0;
+
+        //같은 위치에 있는지 확인
+        for (int i=0; i<GAME_RANDOM_COUNT; i++){
+            for (int j=0; j<GAME_RANDOM_COUNT; j++){
+                if (gameNumList.get(i)==playerNumList.get(j) && i==j){
+                    strike +=1;
+                }
+                if (gameNumList.get(i)==playerNumList.get(j) && i!=j){
+                    ball +=1;
+                }
+            }
+        }
+        return new int[]{strike, ball};
+    }
 }
