c73729aa9374744f94df811c0fe7125393ebae47
전영서 <20wjsdudtj@gmail.com>
2024-03-12 14:26:48 +0900

    Game 기능 추가 게임을 초기화한 후 Player과 Referee를 사용해 진행, display / 랜덤 변수 할당 / 게임 재시작 여부 조회 기능

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -19,21 +19,40 @@ public class Game {
     }
 
     /*
-     * 게임을 초기화한다. (시작할 수 있도록!)
+     * 게임을 초기화한다. (맨 처음에 시작할 수 있도록!)
      */
     public void run(){
-        System.out.println("숫자 야구 게임을 시작합니다.");
         while (true){
+            System.out.println("숫자 야구 게임을 시작합니다.");
             ArrayList<Integer> gameNumList = getRandomNumbers();
-
-            requestNumbers();
-            //player에게 입력을 받는다. -> Player 객체 사용
-
-            // numList와 player 값을 비교 -> Referee 객체 사용
+            boolean game = newGameStart(gameNumList);
+            if (!game) break;
         }
     }
 
 
+    /*
+     * 새로운 게임 할당 로직
+     */
+    public boolean newGameStart(ArrayList<Integer> gameNumList){
+        while (true){
+            //player에게 입력을 받는다. -> Player 객체 사용
+            requestNumbers();
+            ArrayList<Integer> playerNumList = player.getNumbers();
+
+            // numList와 player 값을 비교 -> Referee 객체 사용
+            int[] result = referee.compare(gameNumList, playerNumList);
+            displayResult(result);
+            if (isFULLStrike(result)) {
+                fullStrike();
+                int continueNum = player.getNumberForContinue();
+                if (continueNum == 2) {
+                    return false; //끝냅니다.
+                }
+                return true; //한 번 더 합니다.
+            }
+        }
+    }
 
     /*
      * Random 값을 추출한다.
@@ -46,26 +65,43 @@ public class Game {
                 numList.add(randomNum);
             }
         }
+        System.out.println("numList "+ numList);
         return numList;
     }
 
 
 
     /*
-     * User에게 입력을 요구한다.
+     * User에게 입력을 요구하기 위한 display
      */
 
     public void requestNumbers(){
         System.out.println("숫자를 입력해주세요 : ");
     }
 
-    public void requestGameContinue(){
+
+    public void fullStrike(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료 ");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ");
     }
 
-    public void threeStrike(){
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료 ");
+    public boolean isFULLStrike(int[] result){
+        return result[0]==FULL_STRIKE;
     }
 
+    public void displayResult(int[] result){
+        int strike = result[0];
+        int ball = result[1];
+
+        if (strike>0 && ball>0){
+            System.out.println(ball+"볼 "+strike+"스트라이크");
+        } else if (strike>0){
+            System.out.println(strike+"스트라이크");
+        } else if (strike>0){
+            System.out.println(ball+"볼");
+        } else{
+            System.out.println("낫싱");
+        }
+    }
 
 }
