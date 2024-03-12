175ea19317837f9ad8e807f12721f15c70237af7
전영서 <20wjsdudtj@gmail.com>
2024-03-12 13:23:46 +0900

    Player 기능 추가 player에게 적절한 입력을 받는다. 이 때 잘못된 입력에 있어서는 IllegalArgumentException 발생

--- src/main/java/baseball/Constants.java
+++ src/main/java/baseball/Constants.java
@@ -8,5 +8,8 @@ public class Constants {
     public static final int GAME_RANDOM_NUM_MIN = 1;
     public static final int GAME_RANDOM_NUM_MAX = 9;
 
+    public static final int PLAYER_RE_INPUT_COUNT = 3;
 
+    public static final int GAME_RESTART_NUM = 1;
+    public static final int GAME_FINISH_NUM = 2;
 }
--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,17 +1,58 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+
+import static baseball.Constants.PLAYER_INPUT_COUNT;
+
+import static baseball.Constants.*;
+
 public class Player {
 
-
-    // TODO
-
     /*
-     * player 기 숫자 3개를 입력한다.
+     * player 가 숫자 3개를 입력한다.
      */
+    public ArrayList<Integer> getNumbers() {
+        String input = Console.readLine(); // 사용자의 입력 받음
 
+        if (checkNumForm(input, PLAYER_INPUT_COUNT)) {
+            ArrayList<Integer> playerNumList = new ArrayList<>();
 
+            for (char word : input.toCharArray()) {
+                // 입력된 String 형태를 각각 int로 변환하여 ArrayList에 추가
+                int num = Character.getNumericValue(word);
+                playerNumList.add(num);
+            }
+            return playerNumList;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private boolean checkNumForm(String input, int count){
+        String regex = getInputRegex(GAME_RANDOM_NUM_MIN, GAME_RANDOM_NUM_MAX, GAME_RANDOM_COUNT);
+        return input.length() == count && input.matches(regex);
+    }
 
     /*
      * player 가 게임을 계속할지 말지에 대한 입력한다.
      */
+    public int getNumberForContinue() {
+        String input = Console.readLine();
+        if (checkNumFormForContinue(input, PLAYER_RE_INPUT_COUNT)){
+            return Integer.parseInt(input);
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private boolean checkNumFormForContinue(String input, int count){
+        String regex = getInputRegex(GAME_RESTART_NUM, GAME_FINISH_NUM, count);
+        return input.length() == count && input.matches(regex);
+    }
+
+
+    public String getInputRegex(int minNum, int maxNum, int count){
+        return String.format("^[%d-%d]{%d}$", minNum, maxNum, count);
+    }
+
 }
